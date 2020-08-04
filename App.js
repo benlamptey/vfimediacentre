3d82ef75f60970a98b266bca0e267e736e84fbf0
Ben Lamptey <ben@bigbluedoor.net>
2020-08-04 13:52:13 +0100

    final app completed - BETA VERSION

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
+import  React, {Component, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from './screens/home';
@@ -7,43 +7,50 @@ import ListingPage from './screens/ListingPage';
 import AudioPlayer from './screens/AudioPlayer';
 import {PlayerContextProvider} from './contexts/PlayerContexts';
 import Header from './components/header';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createStackNavigator();
 
-function App() {
+export default class App extends Component {
+    componentDidMount() {
+        // do stuff while splash screen is shown
+        // After having done stuff (such as async tasks) hide the splash screen
+        SplashScreen.hide();
+    }
 
-    return (
-        <PlayerContextProvider>
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen
-                        name="Home"
-                        component={Home}
-                        options={{
-                            headerShown: false,
-                            header: () => <Header
-                                title='VFI Media Centre'
-                                bgImage={require('./assets/images/headerBG.jpg')}/>,
-                        }}/>
-                    <Stack.Screen
-                        name="ListingPage"
-                        component={ListingPage}
-                        options={{
-                            headerShown: false,
-                        }}
-                    />
-                    <Stack.Screen
-                        name="AudioPlayer"
-                        component={AudioPlayer}
-                        options={{
-                            headerShown: false,
-                            headerMode: 'none'
+    render()
+    {
+        return (
+            <PlayerContextProvider>
+                <NavigationContainer>
+                    <Stack.Navigator>
+                        <Stack.Screen
+                            name="Home"
+                            component={Home}
+                            options={{
+                                headerShown: false,
+                                header: () => <Header
+                                    title='VFI Media Centre'
+                                    bgImage={require('./assets/images/headerBG.jpg')}/>,
+                            }}/>
+                        <Stack.Screen
+                            name="ListingPage"
+                            component={ListingPage}
+                            options={{
+                                headerShown: false,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="AudioPlayer"
+                            component={AudioPlayer}
+                            options={{
+                                headerShown: false,
+                                headerMode: 'none'
 
-                        }}/>
-                </Stack.Navigator>
-            </NavigationContainer>
-        </PlayerContextProvider>
-    );
+                            }}/>
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </PlayerContextProvider>
+        );
+    }
 }
-
-export default App;
